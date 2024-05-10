321fc50ec6b545743a34ff950fa65abb20697442
Letícia Fariasf 2b <133364942+laliMaryLeticinha231dsnFabisDil@users.noreply.github.com>
2024-05-10 16:24:10 -0300

    Initial commit

---
+++ Vetor/Vetor.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+
+
+using namespace std;
+int main()
+{
+    //definir vetor do tipo inteiro com valores
+    //vector<int> numeros = { 10, 20, 30, 40, 50 };
+    //
+    ////adiciona novos elementos no vetor
+    //numeros.push_back(60);
+    //numeros.push_back(70);
+    //numeros.push_back(80);
+
+
+    ////i é uma variavel armazena números
+    ////mostra o vetor
+    //for (int i = 0; i <= numeros.size(); i++) {
+    //    cout << numeros[i];
+    //}
+
+
+
+
+    //PARTE DOIS DA EXPLICAÇÃO
+    //vector<int> num(5); 
+    ////PREENCHENDO O VETOR PELO CONSOLE
+    // //INSERINDO ELEMENTOS NO VETOR
+    //cout << "digite 5 numeros para vetor: ";
+    //for (int i = 0; i < 5; i++) {
+    //    cout << "Numero " ; 
+    //    cin >> num[i];
+    //}
+
+    ////MOSTRANDO O VETOR QUE CRIAMOS
+    //for (int i = 0; i < num.size(); i++) {
+    //    cout << num[i];
+    //}
+
+
+    //TERCEIRA PARTE
+    //vector<int> notas = { 10, 8, 7, 9, 5 };
+    //int numeroProcurando = 7;
+    //bool encontrou = false;
+    ////VARRENDO O VETOR
+    //for (int i = 0; i < notas.size(); i++) {
+    //    if (notas[i] == numeroProcurando) {
+    //        encontrou = true;
+    //        break; //PARA O FOR
+    //    }
+    //}
+    //if (encontrou) {
+    //    cout << "O numero foi encontrado";
+    //} 
+    //else {
+    //    cout << "O numero nao foi encontrado";
+    //}
+
+
+    // quarta parte
+    //vector<int> notas = { 10, 8, 7, 9, 5 };
+    ////ORDENANDO O VETOR POR ORDEM CRESCENTE
+    //sort(notas.begin(), notas.end());
+    //for (int num = 0; num < notas.size(); num++) {
+    //    cout << notas[num];
+    //    cout << "\n";
+    //}
+    ////ORDENANDO O VETOR POR ORDEM DECRESENTE
+    //for (int i = 0; i < notas.size(); i++) {
+    //    cout << notas[i];
+    //    cout << "\n";
+    //}
+
+
+    //QUINTA PARTE   MÉDIA
+    //vector<int> notas = { 10, 8, 7, 9, 5 };
+    //double soma = 0;
+    //double media = 0;
+    ////SOMA DOS ELEMENTOS DO VETOR 
+    //for (int i = 0; i < notas.size(); i++) {
+    //    soma += notas[i];
+    //}
+
+    ////CALCULANDO MEDIA DOS ELEMENTO
+
+    //media = soma / notas.size();
+    //cout << "SOMA DOS ELEMENTOS DOS VETORES " << soma;
+    //cout << "\nMedia: " << media;
+
+
+
+
+    //ATIVIDADE
+
+
+    //ATIVIDADE 1
+     /*vector<int> num(10);
+
+
+    cout << "digite 5 numeros para vetor: ";
+    for (int i = 0; i < 10; i++) {
+        cout << "Numero " ;
+        cin >> num[i];
+    }
+    for (int i = 0; i < num.size(); i++) {
+        cout << num[i];
+        cout << "\n";
+    }*/
+
+
+
+
+//ATIVIDADE 2
+
+//vector<int> num(7);
+//int soma = 0;
+//cout << "digite 5 numeros para vetor: ";
+//for (int i = 0; i < 7; i++) {
+//    cout << "Numero ";
+//    cin >> num[i];
+//}
+//for (int i = 0; i < num.size(); i++) {
+//    soma += num[i];
+//}
+//
+//    cout << "SOMA DOS ELEMENTOS DOS VETORES " << soma;
+//    return 0;
+}
+
+
+
+    //ATIVIDDADE 3
+
+    /*vector<int> idades = { 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 18, 18, 18, 17, 17 };
+    double soma = 0;
+    double media = 0;
+
+    for (int i = 0; i < idades.size(); i++) {
+        soma += idades[i];
+    }
+
+
+    media = soma / idades.size();
+    cout << "\nMedia: " << media;
+
+
+   */
+
+
+
+//ATIVIDADE 4
+
+ //vector<int> valores = { 5, 9, 7, 10, 15, 23 };
+ //  double soma = 0;
+ //   double media = 0;
+ //   valores.push_back(20);
+ //   valores.push_back(30);
+
+ //   for (int i = 0; i < valores.size(); i++) {
+ //       soma += valores[i];
+ //   }
+
+
+ //   cout << "SOMA DOS ELEMENTOS DOS VETORES " << soma;
